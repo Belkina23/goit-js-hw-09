c1bfe8ce4f0ab93a3e1923d45c12643b63a4082c
Belkina23 <23zakat@gmail.com>
2022-11-25 18:02:22 +0200

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+}
+
+let tomeId = null;
+
+refs.start.addEventListener('click', () => {
+  tomeId = setInterval(getBackgraundColor, 1000);
+  refs.start.disabled = true;
+});
+
+refs.stop.addEventListener('click', () => {
+  clearInterval(tomeId);
+  refs.start.disabled = false;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function getBackgraundColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
 
